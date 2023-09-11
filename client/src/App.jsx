0e0f54abc131426afe402d22cf526915565815d7
Sauhardha Pandey <sauhardhap@hotmail.com>
2023-09-11 10:14:44 +1000

    added client

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './styles/App.scss';
+
+function App() {
+  return (
+    <div className='Landing'>
+      <div>
+        <h1>Artization Set-Up</h1>
+        <h2>(ᵒ̤̑ ₀̑ ᵒ̤̑)wow!*✰</h2>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
