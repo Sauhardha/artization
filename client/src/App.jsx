3d71f7a5f9d0e83c5af0f503e8e50ccedc2a754f
Sauhardha Pandey <sauhardhap@hotmail.com>
2023-09-13 18:01:02 +1000

    Tailwind-css added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,20 @@
-import './styles/App.scss';
 
 function App() {
   return (
-    <div className='Landing'>
-      <div>
-        <h1>Artization Set-Up</h1>
-        <h2>(ᵒ̤̑ ₀̑ ᵒ̤̑)wow!*✰</h2>
+    <body className='h-screen flex justify-center items-center bgDark'>
+      <div className="light">
+        <h1 className='headFont text-9xl font-bold'>Artization</h1>
+
+        <div className="whitespace-nowrap ">
+          <h2 className="bodyFont text-4xl inline">A <span className="c3">revolution</span> in art <span className="c1">analysis</span></h2>
+        </div>  
+
+        <h3 className="text-center"><br></br>
+        <span className="c6">(ᵒ̤̑ ₀̑ ᵒ̤̑)wow!*</span>
+        <span className="c4">✰</span></h3>
       </div>
       
-    </div>
+    </body>
   );
 }
 
