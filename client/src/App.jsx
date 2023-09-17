d2a48991aa2c5db363cdf97c2a3ac89e0c1c5663
Sauhardha Pandey <sauhardhap@hotmail.com>
2023-09-18 00:47:07 +1000

    LoginPage set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,15 @@
+import React from 'react';
+import LoginPage from './pages/login';
+import { Router, Route, Routes } from "react-router-dom"; //Will use to create routes later
 
 function App() {
+
+
+
   return (
-    <body className='h-screen flex justify-center items-center bgDark'>
-      <div className="light">
-        <h1 className='headFont text-9xl font-bold'>Artization</h1>
-
-        <div className="whitespace-nowrap ">
-          <h2 className="bodyFont text-4xl inline">A <span className="c3">revolution</span> in art <span className="c1">analysis</span></h2>
-        </div>  
-
-        <h3 className="text-center"><br></br>
-        <span className="c6">(ᵒ̤̑ ₀̑ ᵒ̤̑)wow!*</span>
-        <span className="c4">✰</span></h3>
-      </div>
-      
-    </body>
+    <div>
+        <LoginPage />
+    </div>
   );
 }
 
