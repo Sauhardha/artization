9b8c4805df49a1471619f626673e20379625bdf8
Alwin K <100253911+alwin5021@users.noreply.github.com>
2023-09-19 03:11:50 +1000

    Fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
    <div>
     <BrowserRouter>
         <Routes>
-          <Route index element= { <HomePage.jsx/>} />
+          <Route index element= { <HomePage/>} />
           <Route path="/home" element={<HomePage/>} />
           <Route path="/login" element={<LoginPage/>} />
           <Route path="/gallery" element={<GalleryPage/>} />
