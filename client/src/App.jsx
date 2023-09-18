45649f930e3283f5be61185be387465ff25f40b8
Alwin K <100253911+alwin5021@users.noreply.github.com>
2023-09-19 02:21:49 +1000

    Added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import LoginPage from './pages/login';
-import { Router, Route, Routes } from "react-router-dom"; //Will use to create routes later
-
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+import GalleryPage from './pages/GalleryPage';
+// import { Router, Route, Routes } from "react-router-dom"; //Will use to create routes later
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
 
 
 
   return (
-    <div>
-        <LoginPage />
-    </div>
-  );
+    //All components that are nested inside this App component have access to the router
+   <div>
+    <BrowserRouter>
+        <Routes>
+          <Route index element= { <HomePage.jsx/>} />
+          <Route path="/home" element={<HomePage/>} />
+          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/gallery" element={<GalleryPage/>} />
+        </Routes>
+    </BrowserRouter>
+   </div>
+  )
 }
 
 export default App;
