f53432d9009da193a8e4b32c2b772ddda0a85c19
Alper Demirbilek <alperdemirbilek12@gmail.com>
2023-09-19 21:03:34 +1000

    Contact Page draft

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage';
 import GalleryPage from './pages/GalleryPage';
 // import { Router, Route, Routes } from "react-router-dom"; //Will use to create routes later
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
 function App() {
 
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/home" element={<HomePage/>} />
           <Route path="/login" element={<LoginPage/>} />
           <Route path="/gallery" element={<GalleryPage/>} />
+          <Route path="/contact" element={<ContactPage/>} />
         </Routes>
     </BrowserRouter>
    </div>
