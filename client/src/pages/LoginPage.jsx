592ab00fd83afd2d6fc8d7d377046f7a7ff580d3
Sauhardha Pandey <sauhardhap@hotmail.com>
2023-09-26 19:15:34 +1000

    Login and Signup Design Fix

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,70 +1,57 @@
 import React from 'react';
 import '../styles/index.css';
-import {useState} from 'react'
+import { useState } from 'react'
 import { useLogin } from '../hooks/useLogin';
-
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const {login, error, isLoading} = useLogin();
+    const { login, error, isLoading } = useLogin();
 
-    const handleSubmit = async(e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
-
         await login(email, password)
     }
 
     return (
-        <>
-            <body class=" h-screen">
-                <div className='grid grid-cols-12 h-screen'>
-
-
-                    {/* Artization logo on left of page */}
-                    <div className='bgLogo col-span-4 flex justify-center items-center'>
-                        <img src="/ARTIZATION.png" alt="" className='w-80 h-80'/>
-                    </div>
-
-                    {/* Login box on right of page */}
-                    <div className="bg-stone-200 col-span-8 p-8 rounded shadow-md flex justify-center items-center">
-
-                        
-                        <div className='w-96'>
-                            <h1 className="text-2xl dark headFont mb-4">Log in</h1>
-                            <form className='login' onSubmit={handleSubmit}>
-                                <div className="mb-4">
-                                    <label for="email" class="block dark">Email</label>
-                                    <input type="email" id="username" name="username" onChange={(e) => setEmail(e.target.value)} value={email}
-                                    className="w-full bgLogo border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-emerald-300" required></input>
-                                </div>
-                                <div className="mb-6">
-                                    <label for="password" class="block dark">Password</label>
-                                    <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)} value={password}
-                                    className="w-full bgLogo border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-emerald-300" required></input>
-                                </div>
-                                <button disabled={isLoading} type="submit" class="w-full bg-emerald-600 hover:bg-emerald-500 text-white font-semibold py-2 rounded focus:outline-none focus:ring focus:ring-blue-300">
-                                    Log in
-                                </button>
-                                {error && <div className='text-lg'>{error}</div>}
-                            </form>
-                        </div>
-
-
-
-
-
-
-
-
-                    </div>
+        <div className='h-screen flex justify-center items-center bgDark'>
+            <div className="bg-white p-8 rounded shadow-md flex flex-col justify-center items-center">
+                {/* Logo */}
+                <div className=' flex justify-center items-center w-max'>
+                    <img src="/ARTIZATION.png" alt="" className='w-40 h-40' />
                 </div>
 
-            </body>
+                {/* Login Form */}
+                <div className=' p-8 grid grid-cols-2 gap-8 w-auto'>
+                    <img src="/images/aiFace.png" alt="" className='w-64 h-80' />
 
-        </>
+                    <div className='w-auto my-4'>
+                        <h1 className="text-2xl dark headFont mb-4">Log in</h1>
+                        <form className='login' onSubmit={handleSubmit}>
+                            <div className="mb-4">
+                                <label for="email" class="block dark">Email</label>
+                                <input type="email" id="username" name="username" onChange={(e) => setEmail(e.target.value)} value={email}
+                                    className="w-full bg-logo border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-emerald-300" required></input>
+                            </div>
+                            <div className="mb-6">
+                                <label for="password" class="block dark">Password</label>
+                                <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)} value={password}
+                                    className="w-full bg-logo border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-emerald-300" required></input>
+                            </div>
+                            <button disabled={isLoading} type="submit" class="w-full bg-blue-600 hover:bg-emerald-500 text-white font-semibold py-2 rounded focus:outline-none focus:ring focus:ring-blue-300">
+                                Log in
+                            </button>
+                            <div className='p-2'>
+                                <span>Or </span>
+                                <span className="text-blue-400"><a href="/signup">Create an Account.</a></span>
+                            </div>
+                            {error && <span className='text-lg'>{error}</span>}
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
-
 }
 
 export default Login;
--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,70 +1,56 @@
 import React from 'react';
 import '../styles/index.css';
-import {useState} from 'react'
+import { useState } from 'react'
 import { useSignup } from '../hooks/useSignup';
-
 
 const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const {signup, error, isLoading} = useSignup()
+    const { signup, error, isLoading } = useSignup();
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-
-        await signup(email, password);
+        await signup(email, password)
     }
 
     return (
-        <>
-            <body class=" h-screen">
-                <div className='grid grid-cols-12 h-screen'>
-
-
-                    {/* Artization logo on left of page */}
-                    <div className='bgLogo col-span-4 flex justify-center items-center'>
-                        <img src="/ARTIZATION.png" alt="" className='w-80 h-80'/>
-                    </div>
-
-                    {/* Login box on right of page */}
-                    <div class="bg-stone-200 col-span-8 p-8 rounded shadow-md flex justify-center items-center">
-
-                        
-                        <div className='w-96'>
-                            <h1 className="text-2xl dark headFont mb-4">Create Account</h1>
-                            <form className='signup' onSubmit={handleSubmit}>
-                                <div className="mb-4">
-                                    <label for="email" class="block dark">Email</label>
-                                    <input type="email" id="username" name="username" onChange={(e) => setEmail(e.target.value)} value={email}
-                                    className="w-full bgLogo border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-emerald-300" required></input>
-                                </div>
-                                <div class="mb-6">
-                                    <label for="password" class="block dark">Password</label>
-                                    <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)} value={password}
-                                    className="w-full bgLogo border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-emerald-300" required></input>
-                                </div>
-                                <button  type="submit" disabled={isLoading} class="w-full bg-emerald-600 hover:bg-emerald-500 text-white font-semibold py-2 rounded focus:outline-none focus:ring focus:ring-blue-300">
-                                    Sign up
-                                </button>
-                                {error && <div className='flex text-lg text-red-500 linkFont'><p>Error</p></div>}
-                            </form>
-                        </div>
-
-
-
-
-
-
-
-
-                    </div>
+        <div className='h-screen flex justify-center items-center bgDark'>
+            <div className="bg-white p-8 rounded shadow-md flex flex-col justify-center items-center">
+                {/* Logo */}
+                <div className='flex justify-center items-center w-max'>
+                    <img src="/ARTIZATION.png" alt="" className='w-40 h-40' />
                 </div>
 
-            </body>
+                {/* Login Form */}
+                <div className='p-8 grid grid-cols-2 gap-8 w-auto'>
+                    <img src="/images/userPotential.png" alt="" className='self-center md:w-96 md:h-72 w-72 h-40 ' />
 
-        </>
+                    <div className='w-auto my-4'>
+                        <h1 className="text-2xl dark headFont mb-4">Create an Account</h1>
+                        <form className='login' onSubmit={handleSubmit}>
+                            <div className="mb-4">
+                                <label for="email" class="block dark">Email</label>
+                                <input type="email" id="username" name="username" onChange={(e) => setEmail(e.target.value)} value={email}
+                                    className="w-full bg-logo border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-emerald-300" required></input>
+                            </div>
+                            <div className="mb-6">
+                                <label for="password" class="block dark">Password</label>
+                                <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)} value={password}
+                                    className="w-full bg-logo border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-emerald-300" required></input>
+                            </div>
+                            <button disabled={isLoading} type="submit" class="w-full bg-blue-600 hover:bg-emerald-500 text-white font-semibold py-2 rounded focus:outline-none focus:ring focus:ring-blue-300">
+                                Sign Up
+                            </button>
+                            <div className='p-2'>
+                                <span className="text-blue-400"><a href="/login">Have an Account? Log in.</a></span>
+                            </div>
+                            {error && <span className='text-lg'>{error}</span>}
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
-
 }
 
 export default Signup;
