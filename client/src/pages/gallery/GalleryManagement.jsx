bb4931872955a1dcf23e7ec5b761fe2c27588b0f
Sauhardha <sauhardhap@hotmail.com>
2023-11-13 21:52:30 +1100

    Gallery Users UI Admin

--- client/src/pages/gallery/GalleryManagement.jsx
+++ client/src/pages/gallery/GalleryManagement.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { axiosInstance } from '../../context/AuthContext';
 import { useParams } from 'react-router-dom';
+import Navbar from '../../components/Navbar';
 
 
 export default function GalleryManagement() {
@@ -81,24 +82,24 @@ export default function GalleryManagement() {
   
     // Render connected users in a table
     const renderConnectedUsers = () => (
-      <table className="w-full border table-fixed">
+      <table className="w-full self-center box-shadow bg-neutral-50 shadow-xl text-lg m-4 rounded-xl table-fixed">
         <thead>
           <tr>
-            <th className="px-4 py-2 text-left border">User</th>
-            <th className="px-4 py-2 text-left border">Email</th>
-            <th className="px-4 py-2 text-left border">Action</th>
+            <th className="px-4 py-2 text-left ">User</th>
+            <th className="px-4 py-2 text-left ">Email</th>
+            <th className="px-4 py-2 text-left ">Action</th>
           </tr>
         </thead>
-        <tbody className="text-xs">
+        <tbody className="md:text-base text-xs">
           {galleryData.users.map((user, index) => (
             <tr key={user._id}>
-              <td className="px-4 py-2 border">
+              <td className="px-4 py-2 border-t">
                 {user.firstName && user.firstName} {user.lastName && user.lastName}
               </td>
-              <td className="px-4 py-2 border">{user.email}</td>
-              <td className="px-4 py-2 border">
+              <td className="px-4 py-2 border-t">{user.email}</td>
+              <td className="px-10 py-4 border-t border-l">
                 <button
-                  className="px-4 py-2 font-semibold text-red-700 bg-transparent border border-red-500 rounded hover:bg-red-500 hover:text-white hover:border-transparent"
+                  className="px-4 py-2 font-semibold text-white bg-red-500 border border-red-500 rounded hover:bg-red-600 hover:text-white hover:border-transparent"
                   onClick={() => handleRemoveUserClick(user._id)}
                 >
                   Remove user
@@ -111,10 +112,15 @@ export default function GalleryManagement() {
     );
   
     return (
-      <div className="relative m-6">
+      <>
+      <Navbar />
+      <div className="flex flex-col h-screen GalleryPage mt-12 md:mx-20 mx-4">
         {galleryData ? (
           <>
-            <h1 className="mb-2 text-3xl">{galleryData.gallery?.displayName}</h1>
+          <div className="self-start py-8 md:text-5xl text-3xl font-bold headFont">
+          
+            <h1 className="text-sky-950 uppercase">{galleryData.gallery?.displayName}</h1>
+          </div>
             <p>
               <strong>Technical Contact Email:</strong>{' '}
               {galleryData.gallery?.technicalContactEmail}
@@ -130,21 +136,24 @@ export default function GalleryManagement() {
               {/* Search form */}
               <div className="relative w-1/4 mb-2">
                 <input
-                  type="text"
-                  placeholder="Search users by name"
+                   type="text"
+                   placeholder="Search users by name"
+                   className="w-96 px-4 py-3 border rounded-full mb-4 box-shadow shadow-md"
                   value={searchTerm}
                   onChange={(e) => setSearchTerm(e.target.value)}
-                  className="w-full p-2 border rounded-md"
+                 
                 />
+
+                <div className='flex justify-center'>
   
                 {/* Display search results as a list */}
                 {searchResults.users?.length > 0 && (
-                  <ul className="absolute left-0 z-10 p-2 list-none bg-white border rounded top-full">
+                  <ul className=" left-0 z-10 p-2 list-none bg-neutral-50 rounded top-full">
                     {searchResults.users.map((user) => (
                       <li
                         key={user._id}
                         onClick={() => handleUserClick(user._id)}
-                        className="mb-2 border border-gray-300 cursor-pointer hover:bg-gray-200"
+                        className="mb-2  border-gray-300 cursor-pointer hover:bg-gray-200"
                       >
                         <strong className="text-sm">
                           {user.firstName} {user.lastName}
@@ -155,6 +164,7 @@ export default function GalleryManagement() {
                     ))}
                   </ul>
                 )}
+                </div>
               </div>
   
               {/* Display connected users in a table */}
@@ -165,5 +175,6 @@ export default function GalleryManagement() {
           <p>Loading gallery information...</p>
         )}
       </div>
+      </>
     );
   }
