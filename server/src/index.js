3d0845b661d40a4c86ed258db1ebfc25861f234e
Sauhardha <sauhardhap@hotmail.com>
2023-12-11 19:37:34 +1100

    deploy build

--- server/src/index.js
+++ server/src/index.js
@@ -11,11 +11,29 @@ const galleryRoute = require('./routes/Gallery')
 const path = require('path');
 const fs = require('fs');
 
+
 require('dotenv').config();
+
+const _dirname = path.dirname("")
+const buildPath = path.join(_dirname , "../client/build");
 
 //App
 const app = express();
 app.use(express.json())
+
+app.use(express.static(buildPath))
+
+app.get("/*", function(req, res){
+
+    res.sendFile(
+        path.join(_dirname, "../client/build/index.html"),
+        function (err) {
+            if (err) {
+                res.status(500).send(err);
+            }
+        }
+    );
+})
 
 //DB
 mongoose
